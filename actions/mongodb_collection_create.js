0ae48d4b561c7226038255e2d6bf21d9976637f1
Gotowka <89851069+Gotowka@users.noreply.github.com>
2023-06-04 18:48:05 +0200

    Updated for new function unlimited data

--- actions/mongodb_collection_create.js
+++ actions/mongodb_collection_create.js
@@ -10,7 +10,7 @@ module.exports = {
   
     meta: { version: "3.2.0", preciseCheck: true, author: 'Gotowka', authorUrl: 'https://github.com/Gotowka', downloadUrl: 'https://github.com/Gotowka/mydbm/blob/v3/actions/mongodb_collection_create.js' },
   
-    fields: ["database", "cdata", "cname", "value"],
+    fields: ["database", "cname", "cdata"],
   
     html(isEvent, data) {
       return `
@@ -19,22 +19,20 @@ module.exports = {
           <u>Mod Info:</u><br>
           Created by money#6283<br>
           Help: https://discord.gg/apUVFy7SUh<br>
-          Collection Data is limited to 4
       </p>
   </div><br><br>
   <div style="float: left; width: calc(50% - 12px);">
     <span class="dbminputlabel">Database Name</span>
     <input id="database" class="round" type="text">
-    <br>
-    <span class="dbminputlabel">New Collection Data</span>
-    <input id="cdata" class="round" placeholder="name:value name:value ..." type="text">
   </div>
   <div style="float: right; width: calc(50% - 12px);">
     <span class="dbminputlabel">New Collection Name</span>
     <input id="cname" class="round" type="text">
-    <br>
-    <span class="dbminputlabel">Find By Value</span>
-    <input id="value" class="round" type="text">
+  </div>
+  <div style="float: left; width: calc(100% - 12px);">
+  <br>
+  <span class="dbminputlabel">New Collection Data</span>
+  <input id="cdata" class="round" placeholder="name:value name:value ..." type="text">
   </div>`;
     },
   
@@ -45,15 +43,16 @@ module.exports = {
       const data = cache.actions[cache.index];
       const mClient = this.mongoGetClient()
       const base = data.database
+      const colname = data.cname
       const cData = this.evalMessage(data.cdata, cache)
-      const t = cData.split(' ').filter(w => w === ':').length
-      if (t > 4) console.warn('The New Collection Data is limited to 4, more is not created in the data!')
-      const nData = []
-      for (var i = 0; i + 1 === t ;i++) {
-        const t = this.mongoGetSettingData(i, cData)
-        if (i > 3 === false) nData.push(t)
+      const t = cData.split('').filter(w => w === ':').length
+      let nData = {}
+      for (var i = 0; i + 1 !== t ;i++) {
+        const d = this.mongoGetSettingData(i, cData)
+        nData[d.name] = d.value
       }
-      await mClient.collection(base).insertOne({ [nData[0].name]: [nData[0].value], [nData[1].name]: [nData[1].value], [nData[2].name]: [nData[2].value], [nData[3].name]: [nData[3].value] })
+
+      await mClient.db(base).collection(colname).insertOne(nData)
       this.callNextAction(cache)
     },
   
