947b3bf502d4a31ab0bc26484a6caa61ee30906d
Gotowka <89851069+Gotowka@users.noreply.github.com>
2023-06-28 17:44:55 +0200

    Fixed automod create

--- actions/automod_create.js
+++ actions/automod_create.js
@@ -18,8 +18,6 @@ module.exports = {
   
   fields: ["autoName", "autoType", "channel", "storage", "varName", "delete", "alert"],
 
-  actions: [],
-
   html(isEvent, data) {
     return `
     <div>
@@ -28,7 +26,7 @@ module.exports = {
         Created by money#6283<br>
         Help = https://discord.gg/apUVFy7SUh<br>
         Variables:<br>
-        <span class="dbminputlabel">error('disabled', 'create')</span>
+        <span class="dbminputlabel">error('create')</span>
     </p>
 </div><br>
 <div style="float: left; width: 50%;">
@@ -58,10 +56,6 @@ module.exports = {
       glob.changeType = function() {
           const ch = document.getElementById('channel');
           const alert = document.getElementById('alert');
-          const deletem = document.getElementById('delete');
-          if (alert.value === 'true' && deletem.value === 'true') this.actions = ["alert", "delete"];
-          else if (alert.value === 'true') this.actions = ["alert"];
-          else if (deletem.value === 'true') this.actions = ["delete"];
           if (alert.value === 'true') {
               ch.style.display = null;
           } else {
@@ -72,34 +66,27 @@ module.exports = {
 
 
   async action(cache) {
-    console.log('\x1b[30m[\x1b[35mACTION\x1b[30m]: \x1b[33mautomod_create; \x1b[30m[\x1b[32mv1.2\x1b[30m] \x1b[30m(\x1b[36mv3.2.1\x1b[30m)\x1b[0m')
+    console.log('\x1b[30m[\x1b[35mACTION\x1b[30m]: \x1b[33mautomod_create; \x1b[30m[\x1b[32mv1.3\x1b[30m] \x1b[30m(\x1b[36mv3.2.1\x1b[30m)\x1b[0m')
     const data = cache.actions[cache.index];
-    if (!cache.server.features.includes('AUTO_MODERATION')) {
-      this.storeValue('disabled', 1, 'error', cache)
-      this.callNextAction(cache);
-      return;
-    }
+    let functions = []
     const settings = {
+      name: this.evalMessage(data.autoName, cache),
+      eventType: 1,
+      triggerType: 3,
+      enabled: true,
       actions: []
     };
-    const name = this.evalMessage(data.autoName, cache);
-    if (!name) return this.callNextAction(cache);
-    const type = data.autoType;
     const targetChannel = await this.getChannelFromData(data.storage, data.varName, cache);
-    switch(type) {
-      case "spam":
-        settings.triggerType = 3
-        break;
-    }
 
-    if (this.actions.includes('delete')) settings.actions.push({ type: 1 })
-    if (this.actions.includes('alert')) settings.actions.push({ type: 2, metadata: { channel: targetChannel } })
-  
-    settings.name = name;
-    settings.eventType = 1;
-    settings.enabled = true;
+    if (data.alert === true && data.delete === true) functions = ["delete", "alert"];
+    else if (data.alert === true) functions = ["alert"];
+    else if (data.delete === true) functions = ["delete"];
+
+    if (functions.includes('delete')) settings.actions.push({ type: 1 })
+    if (functions.includes('alert')) settings.actions.push({ type: 2, metadata: { channel: targetChannel } })
 
     await cache.server.autoModerationRules.create(settings).catch(er => {
+      console.log(er)
       this.storeValue('create', 1, 'error', cache)
     });
     this.callNextAction(cache);
