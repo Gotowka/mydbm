63d2144f8e633a319c27d7197b8920923875caf8
Gotowka <89851069+Gotowka@users.noreply.github.com>
2023-07-08 22:26:20 +0200

    Updated automod

--- actions/automod_create.js
+++ actions/automod_create.js
@@ -5,18 +5,23 @@ module.exports = {
   section: "Discord",
 
   subtitle(data, presets) {
-    return `Create automod - ${data.autoName}`;
+    let fn
+    if (data.aspam) fn = "spam";
+    else if (data.akeyword) fn = "keyword";
+    else if (data.amentionspam) fn = "mention_spam";
+    else fn = "none";
+    return `Create automod - ${fn}`;
   },
 
   variableStorage(data, varType) {
-		if (1 !== varType) return;
-		let dataType = "Check result 'disable'/'error'";
-		return ['error', dataType];
-	},
+        if (1 !== varType) return;
+        let dataType = "Check result 'channel'/'create'";
+        return ['error', dataType];
+  },
 
   meta: { version: "3.2.1", preciseCheck: true, author: 'Gotowka', authorUrl: 'https://github.com/Gotowka', downloadUrl: 'https://github.com/Gotowka/mydbm/blob/v3/actions/automod_create.js' },
   
-  fields: ["autoName", "autoType", "channel", "storage", "varName", "delete", "alert"],
+  fields: ["spam", "keyword", "mention_spam", "aspam", "akeyword", "amentionspam"],
 
   html(isEvent, data) {
     return `
@@ -25,72 +30,293 @@ module.exports = {
         <u>Mod Info:</u><br>
         Created by money#6283<br>
         Help = https://discord.gg/apUVFy7SUh<br>
-        Variables:<br>
-        <span class="dbminputlabel">error('create')</span>
+        Variables: error('channel', 'create')
     </p>
-</div><br>
-<div style="float: left; width: 50%;">
-<span class="dbminputlabel">Name</span><br>
-<input id="autoName" class="round" type="text">
-<br>
-<span class="dbminputlabel">Type</span><br>
-<select id="autoType" class="round">
-  <option value="spam">Spam</options>
-</select>
-</div>
-<br><br><br><br><br><br><br>
-<div id="channel" style="float: left; padding-top: 8px; width: 100%;">
-  <channel-input dropdownLabel="Source Channel" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></channel-input>
-</div><br><br><br><br>
-<div style="float: left; width: 50%;">
-<dbm-checkbox id="delete" onchange="glob.changeType()" label="BlockMessage"></dbm-checkbox>
-</div>
-<div style="float: right; width: 50%;">
-<dbm-checkbox id="alert" onchange="glob.changeType()" label="SendAlert"></dbm-checkbox>
-</div>`;
-},
+
+  <tab-system style="margin-top: 20px;">
 
 
-  init() {
-      const { document, glob } = this;
-      glob.changeType = function() {
-          const ch = document.getElementById('channel');
-          const alert = document.getElementById('alert');
-          if (alert.value === 'true') {
-              ch.style.display = null;
-          } else {
-              ch.style.display = 'none';
-          }
-      }
+    <tab label="Spam" icon="align left">
+      <dialog-list id="spam" fields='["autoName", "delete", "alert", "autoMsg", "storage", "varName", "autoRole", "autoChannel", "name"]' dialogTitle="Automod Spam" dialogWidth="540" dialogHeight="460" listLabel="Automod Settings" listStyle="height: calc(100vh - 350px);" itemName="spams" itemTextFunction="data.name + ' - Setting'" itemCols="1" itemHeight="30px;" itemStyle="text-align: left; line-height: 30px;">
+    
+        <tab-system>
+    
+          <tab label="Automod" icon="certificate">
+            <div style="float: left; width: calc(50% - 12px); padding: 8px;">
+              <span class="dbminputlabel">Name</span>
+              <input id="autoName" class="round" type="text">
+            </div>
+          </tab>
+    
+          <tab label="Actions" icon="list">
+            <div style="float: left; padding: 8px;">
+              <dbm-checkbox id="delete" label="BlockMessage"></dbm-checkbox>
+            </div>
+
+            <div style="float: right; padding: 8px;">
+              <dbm-checkbox id="alert" label="SendAlert"></dbm-checkbox>
+            </div>
+          </tab>
+
+          <tab label="Actions Settings" icon="list">
+            <div style="float: left; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">customMessage (SendAlert)</span>
+            <input id="autoMsg" class="round" placeholder="Leave blank for default" type="text">
+            </div>
+
+            <div style="float: left; width: calc(100% - 12px); padding: 8px;">
+              <channel-input dropdownLabel="Source Channel" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></channel-input>
+            </div>
+          </tab>
+
+          <tab label="Exempt" icon="certificate">
+            <div style="float: left; width: calc(50% - 12px); padding: 8px;">
+              <span class="dbminputlabel">Exempt Role (ID)</span>
+              <input id="autoRole" class="round" placeholder="Leave blank for none" type="text">
+            </div>
+
+            <div style="float: right; width: calc(50% - 12px); padding: 8px;">
+              <span class="dbminputlabel">Exempt Channel (ID)</span>
+              <input id="autoChannel" class="round" placeholder="Leave blank for none" type="text">
+            </div>
+          </tab>
+
+          <tab label="Settings" icon="user circle">
+            <div style="float: left; width: calc(50% - 12px); padding: 8px;">
+              <span class="dbminputlabel">Name (SETTING)</span>
+              <input id="name" class="round" type="text">
+            </div>
+          </tab>
+    
+        </tab-system>
+      </dialog-list>
+    </tab>
+    
+    
+    <tab label="Keyword" icon="book image">
+      <dialog-list id="keyword" fields='["autoName", "autoFilter", "delete", "timeout", "alert", "autoTime", "autoMsg", "storage", "varName", "autoRole", "autoChannel", "name"]' dialogTitle="Automod Keyword" dialogWidth="540" dialogHeight="460" listLabel="Automod Settings" listStyle="height: calc(100vh - 350px);" itemName="keywords" itemTextFunction="data.name + ' - Setting'" itemCols="1" itemHeight="30px;" itemStyle="text-align: left; line-height: 30px;">
+    
+      <tab-system>
+
+        <tab label="Automod" icon="certificate">
+          <div style="float: left; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">Name</span>
+            <input id="autoName" class="round" type="text">
+          </div>
+  
+          <div style="float: right; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">Filter</span>
+            <input id="autoFilter" class="round" placeholder="WORD WORD WORD ..." type="text">
+          </div>
+        </tab>
+
+        <tab label="Actions" icon="list">
+          <div style="float: left; padding: 8px;">
+            <dbm-checkbox id="delete" label="BlockMessage"></dbm-checkbox>
+            <br>
+            <dbm-checkbox id="timeout" label="Timeout"></dbm-checkbox>
+          </div>
+
+          <div style="float: right; padding: 8px;">
+            <dbm-checkbox id="alert" label="SendAlert"></dbm-checkbox>
+          </div>
+        </tab>
+
+        <tab label="Actions Settings" icon="list">
+          <div style="float: left; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">Time (Timeout)</span>
+            <input id="autoTime" class="round" placeholder="Time in seconds" type="text">
+          </div>
+
+          <div style="float: right; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">customMessage (SendAlert)</span>
+            <input id="autoMsg" class="round" placeholder="Leave blank for default" type="text">
+          </div>
+
+          <div style="float: left; width: calc(100% - 12px); padding: 8px;">
+            <channel-input dropdownLabel="Source Channel" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></channel-input>
+          </div>
+        </tab>
+
+        <tab label="Exempt" icon="certificate">
+          <div style="float: left; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">Exempt Role (ID)</span>
+            <input id="autoRole" class="round" placeholder="Leave blank for none" type="text">
+          </div>
+
+          <div style="float: right; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">Exempt Channel (ID)</span>
+            <input id="autoChannel" class="round" placeholder="Leave blank for none" type="text">
+          </div>
+        </tab>
+
+        <tab label="Settings" icon="user circle">
+          <div style="float: left; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">Name (SETTING)</span>
+            <input id="name" class="round" type="text">
+          </div>
+        </tab>
+
+      </tab-system>
+    </dialog-list>
+  </tab>
+    
+  <tab label="Mention Spam" icon="plane">
+    <dialog-list id="mention_spam" fields='["autoName", "autoLimit", "delete", "timeout", "alert", "autoTime", "autoMsg", "storage", "varName", "autoRole", "autoChannel", "name"]' dialogTitle="Automod Mention Spam" dialogWidth="540" dialogHeight="460" listLabel="Automod Settings" listStyle="height: calc(100vh - 350px);" itemName="mentionspams" itemTextFunction="data.name + ' - Setting'" itemCols="1" itemHeight="30px;" itemStyle="text-align: left; line-height: 30px;">
+    
+      <tab-system>
+
+        <tab label="Automod" icon="certificate">
+          <div style="float: left; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">Name</span>
+            <input id="autoName" class="round" type="text">
+          </div>
+
+          <div style="float: right; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">Mention Limit</span>
+            <input id="autoLimit" class="round" placeholder="Mention limit in message" type="text">
+          </div>
+        </tab>
+
+        <tab label="Actions" icon="list">
+          <div style="float: left; padding: 8px;">
+            <dbm-checkbox id="delete" label="BlockMessage"></dbm-checkbox>
+            <br>
+            <dbm-checkbox id="timeout" label="Timeout"></dbm-checkbox>
+          </div>
+
+          <div style="float: right; padding: 8px;">
+            <dbm-checkbox id="alert" label="SendAlert"></dbm-checkbox>
+          </div>
+        </tab>
+
+        <tab label="Actions Settings" icon="list">
+          <div style="float: left; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">Time (Timeout)</span>
+            <input id="autoTime" class="round" placeholder="Time in seconds" type="text">
+          </div>
+
+          <div style="float: right; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">customMessage (SendAlert)</span>
+            <input id="autoMsg" class="round" placeholder="Leave blank for default" type="text">
+          </div>
+
+          <div style="float: left; width: calc(100% - 12px); padding: 8px;">
+            <channel-input dropdownLabel="Source Channel" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></channel-input>
+          </div>
+        </tab>
+
+        <tab label="Exempt" icon="certificate">
+          <div style="float: left; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">Exempt Role (ID)</span>
+            <input id="autoRole" class="round" placeholder="Leave blank for none" type="text">
+          </div>
+
+          <div style="float: right; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">Exempt Channel (ID)</span>
+            <input id="autoChannel" class="round" placeholder="Leave blank for none" type="text">
+          </div>
+        </tab>
+
+        <tab label="Settings" icon="user circle">
+          <div style="float: left; width: calc(50% - 12px); padding: 8px;">
+            <span class="dbminputlabel">Name (SETTING)</span>
+            <input id="name" class="round" type="text">
+          </div>
+        </tab>
+    
+      </tab-system>
+    </dialog-list>
+  </tab>
+    
+    
+  <tab label="Settings" icon="cogs">
+    <div style="padding: 8px;">
+      <dbm-checkbox style="float: left;" id="aspam" label="Spam" checked></dbm-checkbox>
+    
+      <dbm-checkbox style="float: left;" id="akeyword" label="Keyword" checked></dbm-checkbox>
+    
+      <dbm-checkbox style="float: left;" id="amentionspam" label="MentionSPam" checked></dbm-checkbox>
+    
+      <br><br>
+    
+      <hr class="subtlebar" style="margin-top: 4px; margin-bottom: 4px;">
+    </div>
+  </tab>
+</tab-system>
+    `;
   },
 
 
+  init() {},
+
   async action(cache) {
-    console.log('\x1b[30m[\x1b[35mACTION\x1b[30m]: \x1b[33mautomod_create; \x1b[30m[\x1b[32mv1.3\x1b[30m] \x1b[30m(\x1b[36mv3.2.1\x1b[30m)\x1b[0m')
+    console.log('\x1b[30m[\x1b[35mACTION\x1b[30m]: \x1b[33mautomod_create; \x1b[30m[\x1b[32mv1.4\x1b[30m] \x1b[30m(\x1b[36mv3.2.1\x1b[30m)\x1b[0m')
     const data = cache.actions[cache.index];
-    let functions = []
+    const option = checkAutoMod()
+    const autoMod = data[option.word][0]
     const settings = {
-      name: this.evalMessage(data.autoName, cache),
+      name: this.evalMessage(autoMod.autoName, cache),
       eventType: 1,
-      triggerType: 3,
+      triggerType: option.number,
       enabled: true,
-      actions: []
+      actions: [],
+      triggerMetadata: {}
+    }
+
+    if (autoMod.delete) settings.actions.push({ type: 1 });
+    if (autoMod.alert) {
+      const targetChannel = await this.getChannelFromData(autoMod.storage, autoMod.varName, cache);
+      if (!targetChannel) {
+        this.storeValue('channel', 1, 'error', cache)
+        this.callNextAction(cache);
+      }
+      if (autoMod.autoMsg) settings.actions.push({ type: 2, metadata: { channel: targetChannel, customMessage: this.evalMessage(autoMod.autoMsg, cache) } });
+      else settings.actions.push({ type: 2, metadata: { channel: targetChannel } });
     };
-    const targetChannel = await this.getChannelFromData(data.storage, data.varName, cache);
-
-    if (data.alert === true && data.delete === true) functions = ["delete", "alert"];
-    else if (data.alert === true) functions = ["alert"];
-    else if (data.delete === true) functions = ["delete"];
-
-    if (functions.includes('delete')) settings.actions.push({ type: 1 })
-    if (functions.includes('alert')) settings.actions.push({ type: 2, metadata: { channel: targetChannel } })
+    if (autoMod.timeout) settings.actions.push({ type: 3, metadata: { durationSeconds: autoMod.autoTime ? this.evalMessage(autoMod.autoTime, cache) : 10 } });
+    if (this.evalMessage(autoMod.autoRole, cache)) {
+      let d = this.evalMessage(autoMod.autoRole, cache)
+      d = cache.server.roles.cache.get(d)
+      if (typeof d === 'object') settings.exemptRoles = [d]
+    }
+    if (this.evalMessage(autoMod.autoChannel, cache)) {
+      let d = this.evalMessage(autoMod.autoChannel, cache)
+      d = cache.server.channels.cache.get(d)
+      if (typeof d === 'object') settings.exemptChannels = [d]
+    }
+    if (this.evalMessage(autoMod.autoFilter, cache)) {
+      const d = this.evalMessage(autoMod.autoFilter, cache).split(' ')
+      settings.triggerMetadata.keywordFilter = d
+    }
+    if (settings.triggerType === 'MENTION_SPAM') settings.triggerMetadata.mentionTotalLimit = autoMod.autoLimit ? parseInt(this.evalMessage(autoMod.autoLimit, cache)) : 1
 
     await cache.server.autoModerationRules.create(settings).catch(er => {
       console.log(er)
       this.storeValue('create', 1, 'error', cache)
+      this.callNextAction(cache)
+      return;
     });
+    this.storeValue('none', 1, 'error', cache)
     this.callNextAction(cache);
+
+    function checkAutoMod() {
+      const t = getAutomod(data)
+      if (t !== "none") return t;
+
+      console.log('\x1b[30m[\x1b[31mERROR\x1b[30m]\x1b[0m Automod is required!\x1b[0m');
+      this.callNextAction(cache);
+    }
+
+    function getAutomod() {
+      if (data.aspam) return { number: 3, word: "spam" };
+      else if (data.akeyword) return { number: 1, word: 'keyword' };
+      else if (data.amentionspam) return { number: 5, word: 'mention_spam' };
+      else return "none";
+    }
   },
+
+
 
   mod() {},
 };
