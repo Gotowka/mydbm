2957c289430bb97d6b82bc0492ec5e26e22b2a03
Gotowka <89851069+Gotowka@users.noreply.github.com>
2023-08-06 14:41:13 +0200

    Updated  gstart

--- actions/gstart.js
+++ actions/gstart.js
@@ -25,6 +25,12 @@ module.exports = {
     return `I create the giveaway for ${data.prize}!`;
   },
 
+  variableStorage(data, varType) {
+    if (parseInt(data.storage, 10) !== varType) return;
+    let dataType = "Giveaway Data";
+    return [data.varName, dataType];
+  },
+
   //---------------------------------------------------------------------
   // Action Meta Data
   //
@@ -45,7 +51,7 @@ module.exports = {
   // are also the names of the fields stored in the action's JSON data.
   //---------------------------------------------------------------------
 
-  fields: ["time", "winners", "prize", "msgid"],
+  fields: ["time", "winners", "prize", "msgid", "storage", "varName"],
 
   //---------------------------------------------------------------------
   // Command HTML
@@ -65,28 +71,30 @@ module.exports = {
         <u>Mod Info:</u><br>
         Created by money#6283<br>
         Help: https://discord.gg/apUVFy7SUh<br>
-        Variables: endtime, hoster, winners, prize
     </p>
 </div><br>
 
 <div style="float: left; width: calc(50% - 12px);">
   <span class="dbminputlabel">Time</span>
-  <input id="time" class="round" placeholder="Type: np 1d/1m/1s" type="text">
+  <input id="time" class="round" placeholder="Example 1d/1m/1s" type="text">
 
   <br>
 
   <span class="dbminputlabel">Winners</span>
   <input id="winners" class="round" type="text">
-  
-  <br>
-
+</div>
+<div style="float: right; width: calc(50% - 12px);">
   <span class="dbminputlabel">Prize</span>
   <input id="prize" class="round" type="text">
-  
+
   <br>
 
   <span class="dbminputlabel">Msg ID</span>
   <input id="msgid" class="round" type="text">
+</div>
+<div style="float: left; width: calc(100% - 12px);">
+  <br>
+  <store-in-variable dropdownLabel="Store Giveaway In" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></store-in-variable>
 </div>
 `;
   },
@@ -110,7 +118,7 @@ module.exports = {
   //---------------------------------------------------------------------
 
   async action(cache) {
-    console.log('ACTION: gstart; [v1.0] (v2.1.9)')
+    console.log('ACTION: gstart; [v1.1] (v2.1.8)')
     const { interaction, msg } = cache;
     const giveaways = require('../data/giveaways.json')
     const fs = require('fs')
@@ -119,40 +127,44 @@ module.exports = {
 
     giveaways[mess.guild.id] = [];
     
-  let duration = this.evalMessage(data.time, cache)
+    let duration = this.evalMessage(data.time, cache)
 
-  if (duration.includes("s")) {
+    if (duration.includes("s")) {
       duration = duration.split("s")[0] * 1000;
-  } else if (duration.includes("m")) {
+    } else if (duration.includes("m")) {
       duration = duration.split("m")[0] * 60000;
-  } else if (duration.includes("h")) {
+    } else if (duration.includes("h")) {
       duration = duration.split("h")[0] * 3600000;
-  } else if (duration.includes("d")) {
+    } else if (duration.includes("d")) {
       duration = duration.split("d")[0] * 86400000;
-  } else {
+    } else {
       duration = duration * 1000;
-  };
-  const endtime2 = new Date().getTime() + duration;
-  const endtime = Date.parse(new Date(new Date().getTime() + duration)) / 1000;
+    };
+    const endtime2 = new Date().getTime() + duration;
+    const endtime = Date.parse(new Date(new Date().getTime() + duration)) / 1000;
 
-  giveaways[mess.guild.id].push({
-    "guild": mess.guild.id,
-    "channel": mess.channel.id,
-    "hoster": mess.member.user.tag,
-    "winners": this.evalMessage(data.winners, cache),
-    "prize": this.evalMessage(data.prize, cache),
-    "end": endtime2,
-    "msg": this.evalMessage(data.msgid, cache),
-    "members": [],
-    "ended": false,
-});
+    const gData = {
+      winners: this.evalMessage(data.winners, cache),
+      host: mess.member,
+      prize: this.evalMessage(data.prize, cache),
+      end: endtime
+    }
 
-fs.writeFileSync("./data/giveaways.json", JSON.stringify(giveaways));
-this.storeValue(`<t:${endtime}:R>`, 1, 'endtime', cache)
-this.storeValue(this.evalMessage(data.winners, cache), 1, 'winners', cache)
-this.storeValue(this.evalMessage(data.prize, cache), 1, 'prize', cache)
-this.storeValue(mess.member.user.tag, 1, 'hoster', cache)
-this.callNextAction(cache)
+    giveaways[mess.guild.id].push({
+      "guild": mess.guild.id,
+      "channel": mess.channel.id,
+      "host": mess.member,
+      "winners": gData.winners,
+      "prize": gData.prize,
+      "end": endtime2,
+      "msg": this.evalMessage(data.msgid, cache),
+      "members": [],
+      "ended": false,
+    });
+
+    fs.writeFileSync("./data/giveaways.json", JSON.stringify(giveaways));
+    this.storeValue(gData, parseInt(data.storage, 10), data.varName, cache)
+    this.callNextAction(cache)
 
   },
   //---------------------------------------------------------------------
