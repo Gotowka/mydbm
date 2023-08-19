ad90ce09c79c90e51d6e8fb5144bdfaa914e22d8
Gotowka <89851069+Gotowka@users.noreply.github.com>
2023-08-19 07:12:38 +0200

    Small change

--- mods/v2/send_image.js
+++ mods/v2/send_image.js
@@ -82,7 +82,7 @@ module.exports = {
 
 <div style="padding-top: 8px;">
 	<span class="dbminputlabel">Message</span><br>
-	<textarea id="message" class="dbm_monospace" rows="7" placeholder="Insert message here..." style="white-space: nowrap; resize: none;"></textarea>
+	<textarea id="message" class="dbm_monospace" rows="5" placeholder="Insert message here..." style="white-space: nowrap; resize: none;"></textarea>
 </div>
 
 <br>
